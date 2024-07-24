c63e28644831c5b55f279ead7e30fb6460d6f1f9
David (AusOcean) <160693812+ao-david@users.noreply.github.com>
2024-07-24 17:31:01 +0930

    you Itoa

--- model/factories.go
+++ model/factories.go
@@ -106,7 +106,7 @@ func WaterTemperatureSensor() SensorV2 {
 func NewAlarmNetworkVar(numberOfFailures int) *Variable {
 	return &Variable{
 		Name:  "AlarmNetwork",
-		Value: strconv.Atoi(numberOfFailures),
+		Value: strconv.Itoa(numberOfFailures),
 	}
 }
 
@@ -124,7 +124,7 @@ func NewAlarmPeriodVar(duration time.Duration) *Variable {
 func NewAlarmRecoveryVoltageVar(threshold int) *Variable {
 	return &Variable{
 		Name:  "AlarmRecoveryVoltage",
-		Value: strconv.Atoi(threshold),
+		Value: strconv.Itoa(threshold),
 	}
 }
 
@@ -133,7 +133,7 @@ func NewAlarmRecoveryVoltageVar(threshold int) *Variable {
 func NewAlarmVoltageVar(threshold int) *Variable {
 	return &Variable{
 		Name:  "AlarmVoltage",
-		Value: strconv.Atoi(threshold),
+		Value: strconv.Itoa(threshold),
 	}
 }
 
@@ -178,7 +178,7 @@ func NewPower3Var(power bool) *Variable {
 func NewPulsesVar(pulses int) *Variable {
 	return &Variable{
 		Name:  "Pulses",
-		Value: strconv.Atoi(pulses),
+		Value: strconv.Itoa(pulses),
 	}
 }
 
